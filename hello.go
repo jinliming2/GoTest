414036ff6a19689c290b620aca8696d6552c74e8
Liming Jin <jinlm@knownsec.com>
2017-05-21 22:18:54 +0800

    Hello World🌎

---
+++ hello.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+    "os"
+    "strings"
+    "fmt"
+)
+
+func main() {
+    who := "World"
+    if len(os.Args) > 1 {
+        who = strings.Join(os.Args[1:], " ")
+    }
+    fmt.Println("Hello", who)
+}
